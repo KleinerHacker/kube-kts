9f25d2f9268fd3f51bed400dd3a515464d535f52
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2026-04-15 19:13:22 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "2.3.10" apply false
+}
+
+group = "org.pcsoft.tooling"
+version = "1.0-SNAPSHOT"
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
