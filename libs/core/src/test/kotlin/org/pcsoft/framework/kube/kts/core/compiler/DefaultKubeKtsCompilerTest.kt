21ef5c213aa1b25f19ae7bc0eb4ec94c926489da
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2026-04-19 21:38:56 +0200

    insert test for service spec and update README.md

--- libs/core/src/test/kotlin/org/pcsoft/framework/kube/kts/core/compiler/DefaultKubeKtsCompilerTest.kt
+++ libs/core/src/test/kotlin/org/pcsoft/framework/kube/kts/core/compiler/DefaultKubeKtsCompilerTest.kt
@@ -4,11 +4,16 @@ import org.apache.commons.io.IOUtils
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.pcsoft.framework.kube.kts.api.chart.ChartSpec
+import org.pcsoft.framework.kube.kts.api.chart.resources.ServiceSpec
+import org.pcsoft.framework.kube.kts.api.chart.resources.types.PortSpec
+import org.pcsoft.framework.kube.kts.api.chart.template.TemplateSpec
 import org.pcsoft.framework.kube.kts.api.chart.types.DependencySpec
 import org.pcsoft.framework.kube.kts.api.chart.types.KubeVersion
 import org.pcsoft.framework.kube.kts.core.KubeKtsFile
 import org.pcsoft.framework.kube.kts.core.StringKubeKzsFile
 import java.net.URI
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
 class DefaultKubeKtsCompilerTest {
     companion object {
@@ -16,7 +21,7 @@ class DefaultKubeKtsCompilerTest {
     }
 
     @Test
-    fun testSuccessfully() {
+    fun testChart() {
         val script = IOUtils.resourceToString("/helm/chart.kts", Charsets.UTF_8)
 
         val compiledScript = compiler.compile(StringKubeKzsFile(script, KubeKtsFile.Type.CHART))
@@ -83,6 +88,60 @@ class DefaultKubeKtsCompilerTest {
         Assertions.assertEquals("appVersion", chartSpec.appVersion)
         Assertions.assertEquals(true, chartSpec.deprecated)
         Assertions.assertEquals(mapOf("annotation" to "value"), chartSpec.annotations)
+    }
+
+    @Test
+    fun testService() {
+        val script = IOUtils.resourceToString("/helm/templates/service.kts", Charsets.UTF_8)
+
+        val compiledScript = compiler.compile(StringKubeKzsFile(script, KubeKtsFile.Type.CHART))
+        Assertions.assertNotNull(compiledScript)
+
+        val serviceSpec = compiler.execute<TemplateSpec<ServiceSpec>>(compiledScript)
+        Assertions.assertNotNull(serviceSpec)
+
+        Assertions.assertNotNull(serviceSpec.metadata)
+        Assertions.assertEquals("metadata", serviceSpec.metadata.name)
+        Assertions.assertEquals("namespace", serviceSpec.metadata.namespace)
+        Assertions.assertEquals("generateName", serviceSpec.metadata.generateName)
+
+        Assertions.assertNotNull(serviceSpec.spec)
+        Assertions.assertEquals(ServiceSpec.Type.LoadBalancer, serviceSpec.spec.type)
+
+        Assertions.assertEquals(1, serviceSpec.spec.ports.size)
+        Assertions.assertEquals("port", serviceSpec.spec.ports[0].name)
+        Assertions.assertEquals(9999, serviceSpec.spec.ports[0].port)
+        Assertions.assertEquals(8888, serviceSpec.spec.ports[0].targetPort)
+        Assertions.assertEquals(7777, serviceSpec.spec.ports[0].nodePort)
+        Assertions.assertEquals(PortSpec.Protocol.SCTP, serviceSpec.spec.ports[0].protocol)
+        Assertions.assertEquals("https", serviceSpec.spec.ports[0].appProtocol)
+
+        Assertions.assertEquals("clusterIP", serviceSpec.spec.clusterIP)
+        Assertions.assertEquals(listOf("clusterIP"), serviceSpec.spec.clusterIPs)
+
+        Assertions.assertEquals(setOf(ServiceSpec.IPFamily.IPv4, ServiceSpec.IPFamily.IPv6), serviceSpec.spec.ipFamilies)
+        Assertions.assertEquals(ServiceSpec.FamilyPolicy.RequireDualStack, serviceSpec.spec.ipFamilyPolicy)
+
+        Assertions.assertEquals(listOf("externalIP"), serviceSpec.spec.externalIPs)
+        Assertions.assertEquals("externalName", serviceSpec.spec.externalName)
+
+        Assertions.assertEquals(ServiceSpec.TrafficPolicy.Local, serviceSpec.spec.externalTrafficPolicy)
+        Assertions.assertEquals(ServiceSpec.TrafficPolicy.Local, serviceSpec.spec.internalTrafficPolicy)
+
+        Assertions.assertEquals(false, serviceSpec.spec.allocateLoadBalancerNodePorts)
+        Assertions.assertEquals("loadBalancerIP", serviceSpec.spec.loadBalancerIP)
+        Assertions.assertEquals("loadBalancerClass", serviceSpec.spec.loadBalancerClass)
+        Assertions.assertEquals(listOf("loadBalancerSourceRange"), serviceSpec.spec.loadBalancerSourceRanges)
+
+        Assertions.assertEquals(ServiceSpec.SessionAffinity.None, serviceSpec.spec.sessionAffinity)
+        Assertions.assertEquals(
+            ServiceSpec.SessionAffinityConfig(ServiceSpec.ClientIPConfig(30.seconds.toJavaDuration())),
+            serviceSpec.spec.sessionAffinityConfig
+        )
+
+        Assertions.assertEquals(true, serviceSpec.spec.publishNotReadyAddresses)
+        Assertions.assertEquals(3000, serviceSpec.spec.healthCheckNodePort)
+        Assertions.assertEquals(ServiceSpec.TrafficDistribution.PreferClose, serviceSpec.spec.trafficDistribution)
     }
 
 }
--- libs/core/src/test/resources/helm/templates/service.kts
+++ libs/core/src/test/resources/helm/templates/service.kts
@@ -1,3 +1,45 @@
-service {
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
+service {
+    metadata("metadata") {
+        namespace = "namespace"
+        generateName = "generateName"
+    }
+
+    spec {
+        type = ServiceSpec.Type.LoadBalancer
+
+        addPort("port") {
+            port = 9999
+            targetPort = 8888
+            nodePort = 7777
+            protocol = PortSpec.Protocol.SCTP
+            appProtocol = "https"
+        }
+
+        addClusterIP("clusterIP")
+
+        addIpFamily(ServiceSpec.IPFamily.IPv4)
+        addIpFamily(ServiceSpec.IPFamily.IPv6)
+        ipFamilyPolicy = ServiceSpec.FamilyPolicy.RequireDualStack
+
+        addExternalIP("externalIP")
+        externalName = "externalName"
+
+        externalTrafficPolicy = ServiceSpec.TrafficPolicy.Local
+        internalTrafficPolicy = ServiceSpec.TrafficPolicy.Local
+
+        allocateLoadBalancerNodePorts = false
+        loadBalancerIP = "loadBalancerIP"
+        loadBalancerClass = "loadBalancerClass"
+        addLoadBalancerSourceRange("loadBalancerSourceRange")
+
+        sessionAffinity = ServiceSpec.SessionAffinity.None
+        sessionAffinityClientTimeout = 30.seconds.toJavaDuration()
+
+        publishNotReadyAddresses = true
+        healthCheckNodePort = 3000
+        trafficDistribution = ServiceSpec.TrafficDistribution.PreferClose
+    }
 }
